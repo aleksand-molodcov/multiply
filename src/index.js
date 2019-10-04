8d606cd2057faf282282d42e9825f82f8d619803
Anastasiya Molodtsova <molodtsova@mercury.office>
2019-10-04 10:45:28 +0400

    feat: add multiply function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+let f = first.split('').reverse();
+let s = second.split('').reverse();
+let stack = [];
+for (let i = 0; i < f.length; i++) {
+  for (let j = 0; j < s.length; j++) {
+    let m = f[i] * s[j];
+    stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+  }
+}
+
+for (var i = 0; i < stack.length; i++) {
+  var num = stack[i] % 10;
+  var move = Math.floor(stack[i] / 10);
+  stack[i] = num;
+
+  if (stack[i + 1])
+    stack[i + 1] += move;
+  else if (move != 0)
+    stack[i + 1] = move;
+}
+
+
+return stack.reverse().join('');
+
 }
